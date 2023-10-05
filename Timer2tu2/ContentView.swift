c35530dff98cd0b88d0a2a56e02b7c814ac4bd25
morisum <moris@r.sannet.ne.jp>
2023-10-05 15:41:11 +0900

    完成！ onTapGestureを使ってタップすると開始するようにした 残り時間が０より小さいとストップするようにした。＝＝０だと負の時間を許可してしまう。

--- Timer2tu2/ContentView.swift
+++ Timer2tu2/ContentView.swift
@@ -14,10 +14,47 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            TimerView()
         }
         .padding()
     }
 }
+struct TimerView: View {
+    @State private var isTimerRunning = false
+    @State private var currentTimer = 5 // 最初のタイマーの秒数
+    @State private var nextTimer = 2 // 次のタイマーの秒数
+    
+    var body: some View {
+        Text("Current Timer: \(currentTimer)")
+            .font(.largeTitle)
+            .onTapGesture {
+                startTimer()
+            }
+    }
+    
+    func startTimer() {
+        if !isTimerRunning {
+            isTimerRunning = true
+            Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { timer in
+                currentTimer -= 1
+                
+                if currentTimer < 0 {
+                    timer.invalidate()
+                // 現在のタイマーが終了したら、次のタイマーを開始
+                    startNextTimer()
+                }
+            }
+        }
+    }
+    
+    func startNextTimer() {
+        isTimerRunning = false
+        currentTimer = nextTimer
+        nextTimer = 0 // 次のタイマーが必要なければ0に設定
+        startTimer()
+    }
+}
+
 
 #Preview {
     ContentView()
